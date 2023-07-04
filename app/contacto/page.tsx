1754fa2b089bdb9b84ab73f9b293a966e75e3e24
Cristian Ferreyra <cristianbferreyra@gmail.com>
2023-07-04 20:54:18 -0300

    formulario

--- app/contacto/page.tsx
+++ app/contacto/page.tsx
@@ -1,27 +1,42 @@
-import './contacto.css'
+import "./contacto.css";
+{
+  /*
+                Action: Dirección de a dónde va el form
+                    Archivo que procesa el formulario
+                
+                Method: POST / GET (default)
+                    GET: Ideal para búsquedas y algunas APIs (PokeApi)
+                    POST: formularios de contacto, autenticación, registro de usuarios
 
-export default function Contacto () {
-    return (
-        <>
-            <h1>Estás en contacto</h1>
+            */
+}
+<form name="contacto" data-netlify="true" netlify-honeypot="bot-field" hidden>
+  <input type="text" name="nombre" />
+  <input type="email" name="email" />
+  <textarea name="comentario"></textarea>
+</form>;
+export default function Contacto() {
+  return (
+    <>
+      <h1>Estás en contacto</h1>
 
-            <form action="" method="" className='formulario'>
-                <label>
-                    Nombre:
-                    <input type="text" />
-                </label>
-                <label>
-                    Mail:
-                    <input type="email" />
-                </label>
-                <label>
-                    Comentarios:
-                    <textarea></textarea>
-                </label>
+      <form action="" method="post" className="formulario">
+        <label>
+          Nombre:
+          <input type="text" name="nombre" />
+        </label>
+        <label>
+          Mail:
+          <input type="email" name="email" />
+        </label>
+        <label>
+          Comentarios:
+          <textarea name="comentario"></textarea>
+        </label>
 
-                {/* <input type="submit" value="Enviar" /> */}
-                <button type="submit">Enviar</button>
-            </form>
-        </>
-    )
+        {/* <input type="submit" value="Enviar" /> */}
+        <button type="submit">Enviar</button>
+      </form>
+    </>
+  );
 }
